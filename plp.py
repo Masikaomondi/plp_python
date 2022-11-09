8a3f516d0b200f2bda53b2f48d3a181ec0525a83
Masikaomondi <108226877+Masikaomondi@users.noreply.github.com>
2022-11-09 20:01:54 +0300

    Add files via upload

---
+++ plp.py
@@ -0,0 +1,25 @@
+num1 = 0
+num2 = 1
+if num1 > num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
+
+marks = 50
+if marks >= 80 and marks <= 100:
+    print("Grade is A")
+
+elif marks >= 70 and marks <= 79:
+    print("Grade is B")
+
+elif marks >= 60 and marks <= 69:
+    print("Grade is C")
+
+elif marks >= 50 and marks <= 59:
+    print("Grade is D")
+
+elif marks >= 40 and marks <= 49:
+    print("Fail")
+
+else:
+    print("Out of range")
