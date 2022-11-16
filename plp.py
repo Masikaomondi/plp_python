7612a8c06ee1b0ee43ce88fd548bb9b7c76aaef6
Masikaomondi <108226877+Masikaomondi@users.noreply.github.com>
2022-11-16 19:06:15 +0300

    Add files via upload

--- plp.py
+++ plp.py
@@ -70,10 +70,18 @@ salary = eval(input("Enter your salary:"))
 service_yrs = eval(input("Enter Years of service:"))
 
 if service_yrs > 5:
-print("Yours salary(+Bonus) = ",salary + (salary)*5/100)
+   print("Yours salary(+Bonus) = ",salary + (salary)*5/100)
 
 else:
-print("You are not eligible for bonus as you have less service years.")
+    print("You are not eligible for bonus as you have less service years.")
+
+#Functions
+def greetings ():
+    print("Hello Africa, Welcome to PLP Cohort 2")
+
+greetings ()
+
+
 
 
 
