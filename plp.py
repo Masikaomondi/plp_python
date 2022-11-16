f9c25b0a8ef291e8d37e98476d8c43f21828016b
Masikaomondi <108226877+Masikaomondi@users.noreply.github.com>
2022-11-16 15:18:26 +0300

    Add files via upload

--- plp.py
+++ plp.py
@@ -64,5 +64,17 @@ y= int (input("Enter 2nd number"))
 z = x + y
 print(z)
 
+#Self Test Assignment
+salary = eval(input("Enter your salary:"))
+
+service_yrs = eval(input("Enter Years of service:"))
+
+if service_yrs > 5:
+print("Yours salary(+Bonus) = ",salary + (salary)*5/100)
+
+else:
+print("You are not eligible for bonus as you have less service years.")
+
+
 
 
